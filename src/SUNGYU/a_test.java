ea83c243ee6028cf2304681be3dfd5e3a259218f
sungyujeon <sungyujeon91@gmail.com>
2020-11-19 15:02:25 +0900

    Solve 1.Test

--- src/SUNGYU/a_test.java
+++ src/SUNGYU/a_test.java
@@ -1,10 +1,26 @@
 package SUNGYU;
 
-public class a_test {
+import java.util.Scanner;
 
+public class a_Test {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//1.입출력
+		System.out.println("Hello World!");
+		
+		//2.if문(두 수 비교하기)
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		String result = "";
+		if(A < B) {
+			result = "<";
+		}else if(A > B) {
+			result = ">";
+		}else {
+			result = "==";
+		}
+		System.out.println(result);
 	}
 
 }
